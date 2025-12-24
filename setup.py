4d4caf720a0dd6a98da22d1013863ed740ec45d9
KHARVA JENISH H <kenykharva@gmail.com>
2025-12-24 14:37:23 +0530

    ADDED VENV AND SETUP FOR MONGO DB

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="JENISH KHARVA",
+    author_email="22aikha028@ldce.ac.in",
+    packages=find_packages()
+)
